575934d44e2017a90c796c53c21a00a208c0db23
Darshan625 <91241623+Darshan625@users.noreply.github.com>
2025-09-15 11:26:21 +0530

    Update helloworld.groovy

--- vars/helloworld.groovy
+++ vars/helloworld.groovy
@@ -1,4 +1,4 @@
 def call() {
-  echo " Hi $(name) welcome to jenins shared librarie!"
+  echo " Hi ${name} welcome to jenins shared librarie!"
   echo "You are part of the ${team} team, working as ${project} engineer."
 }
